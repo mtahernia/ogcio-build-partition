a639ad74547ed21c175f5e8934a703aa3021d544
oscar <shdoskar@gmail.com>
2021-01-26 17:51:14 +0800

    change parition size & how package build

--- 1a_make_files.sh
+++ 1a_make_files.sh
@@ -10,7 +10,7 @@ RELEASE=buster
 ## Step 1: Download Linux
 
 mkdir -p ${ROOT_FOLDER}
-sudo debootstrap ${RELEASE} ${ROOT_FOLDER} ${APT_MIRROR}
+sudo debootstrap --include=sudo,locales-all ${RELEASE} ${ROOT_FOLDER} ${APT_MIRROR}
 
 
 ## Step 2: Configure Linux
@@ -34,59 +34,59 @@ deb http://deb.debian.org/debian-security stable/updates main
 deb-src http://deb.debian.org/debian-security stable/updates main
 EOF"
 sudo chroot ${ROOT_FOLDER} bash -c "echo 'nameserver 8.8.8.8' > /etc/resolv.conf"
-sudo chroot ${ROOT_FOLDER} bash -c "chmod 1777 /tmp"
-sudo chroot ${ROOT_FOLDER} bash -c "chmod u+s /bin/ping"
-sudo chroot ${ROOT_FOLDER} bash -c "cat << EOF > /etc/locale.gen
-en_US.UTF-8 UTF-8
-en_HK.UTF-8 UTF-8
-EOF"
+#sudo chroot ${ROOT_FOLDER} bash -c "chmod 1777 /tmp"
+#sudo chroot ${ROOT_FOLDER} bash -c "chmod u+s /bin/ping"
+# sudo chroot ${ROOT_FOLDER} bash -c "cat << EOF > /etc/locale.gen
+# en_US.UTF-8 UTF-8
+# en_HK.UTF-8 UTF-8
+# EOF"
 sudo chroot ${ROOT_FOLDER} bash -c "echo Alpha > /etc/hostname"
 sudo chroot ${ROOT_FOLDER} bash -c "echo '127.0.0.1       Alpha' >> /etc/hosts"
 
 
 sudo chroot ${ROOT_FOLDER} bash -c "apt update"
-sudo chroot ${ROOT_FOLDER} bash -c "apt install -y locales && locale-gen"
+# sudo chroot ${ROOT_FOLDER} bash -c "apt install -y locales && locale-gen"
 sudo chroot ${ROOT_FOLDER} bash -c "apt install -y linux-image-amd64 grub-efi-amd64"
 
 
-sudo chroot ${ROOT_FOLDER} bash -c "apt install -y sudo"
-sudo chroot ${ROOT_FOLDER} bash -c "chown root:root /usr/bin/sudo && chmod 4755 /usr/bin/sudo"
-sudo chroot ${ROOT_FOLDER} bash -c "useradd nclab && usermod -aG sudo,netdev nclab"		    # add user nclab and add it to group sudo
-sudo chroot ${ROOT_FOLDER} bash -c "sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd"		    # use bash as default shell
-sudo chroot ${ROOT_FOLDER} bash -c "mkdir /home/nclab && chown nclab.nclab /home/nclab"		# create home directory for nclab
-sudo chroot ${ROOT_FOLDER} bash -c "cp -p /etc/skel/.profile /home/nclab/.bash_profile"
+#sudo chroot ${ROOT_FOLDER} bash -c "apt install -y sudo"
+#sudo chroot ${ROOT_FOLDER} bash -c "chown root:root /usr/bin/sudo && chmod 4755 /usr/bin/sudo"
+sudo chroot ${ROOT_FOLDER} bash -c "useradd -m -s /bin/bash nclab && usermod -aG sudo,netdev nclab"		    # add user nclab and add it to group sudo
+#sudo chroot ${ROOT_FOLDER} bash -c "sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd"		    # use bash as default shell
+#sudo chroot ${ROOT_FOLDER} bash -c "mkdir /home/nclab && chown nclab.nclab /home/nclab"		# create home directory for nclab
+#sudo chroot ${ROOT_FOLDER} bash -c "cp -p /etc/skel/.profile /home/nclab/.bash_profile"
 sudo chroot ${ROOT_FOLDER} bash -c "cat << EOF >> /home/nclab/.bashrc
 # colorized ls
-export LS_OPTIONS='--color=auto'
-eval \`dircolors\`
-alias ls='ls \$LS_OPTIONS'
-alias ll='ls \$LS_OPTIONS -l'
-alias l='ls \$LS_OPTIONS -lA'
+#export LS_OPTIONS='--color=auto'
+#eval \\\`dircolors\\\`
+#alias ls='ls \$LS_OPTIONS'
+#alias ll='ls \$LS_OPTIONS -l'
+#alias l='ls \$LS_OPTIONS -lA'
 
 # colorized PS1
-PS1='\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+#PS1='\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 
 # history format
 HISTTIMEFORMAT='%Y-%m-%d %T  '
 EOF"
 
-
-sudo chroot ${ROOT_FOLDER} bash -c "apt install -y git man bash-completion pciutils usbutils wireless-tools iw ssh hostapd"
+#106
+sudo chroot ${ROOT_FOLDER} bash -c "apt install -y git man bash-completion pciutils usbutils ssh net-tools bridge-utils sshpass zip psmisc wget"
 sudo chroot ${ROOT_FOLDER} bash -c "systemctl enable ssh"
+
+#117
+# Install required pakages
+sudo chroot ${ROOT_FOLDER} bash -c "apt install -y wireless-tools iw wpasupplicant hostapd lm-sensors python-pip libnetfilter-queue-dev quagga nodejs npm"
 sudo chroot ${ROOT_FOLDER} bash -c "systemctl disable hostapd"
 
-
-# Install required pakages
-sudo chroot ${ROOT_FOLDER} bash -c "apt install -y net-tools bridge-utils \
-wpasupplicant lm-sensors python-pip libnetfilter-queue-dev sshpass quagga nodejs zip npm psmisc"
-
+#164
 # Install debug pakages
-sudo chroot ${ROOT_FOLDER} bash -c "apt install -y netcat telnet iperf3 speedometer \
-tcpdump tree traceroute pv"
+sudo chroot ${ROOT_FOLDER} bash -c "apt install -y netcat telnet iperf3 speedometer tcpdump tree traceroute pv"
 
 # Install dev packages packets
 #sudo chroot ${ROOT_FOLDER} bash -c "apt install -y stress parted cmake gcc g++ vim screen gdb valgrind"
 
+#165
 # Install pip packages
 sudo chroot ${ROOT_FOLDER} bash -c "pip install schedule datetime"
 
--- 3b_restore_config.sh
+++ 3b_restore_config.sh
@@ -2,20 +2,26 @@
 
 set -ex
 
-CONF_FOLDER=~/conf-t4
+ROOT_FS=/dev/sdb3
+CONF_FOLDER=~/conf/conf-t0
 DST_FOLDER=/mnt
 
-mkdir -p $DST_FOLDER
+sudo mount ${ROOT_FS} ${DST_FOLDER}
 
-sudo cp -p $CONF_FOLDER/bridge.sh $DST_FOLDER/home/nclab/bridge/
-sudo cp -p $CONF_FOLDER/check_hostapd.sh $DST_FOLDER/home/nclab/hostapd/
-sudo cp -p $CONF_FOLDER/check_wpa.sh $DST_FOLDER/home/nclab/wpa/
-sudo cp -p $CONF_FOLDER/default.conf $DST_FOLDER/home/nclab/wpa/
-sudo cp -p $CONF_FOLDER/default.conf $DST_FOLDER/home/nclab/wpa/wpa_supplicant.conf
-sudo cp -p $CONF_FOLDER/hopover.conf $DST_FOLDER/home/nclab/wpa/
-sudo cp -p $CONF_FOLDER/hostapd.conf $DST_FOLDER/home/nclab/hostapd/
-sudo cp -p $CONF_FOLDER/hostname $DST_FOLDER/etc/
-sudo cp -p $CONF_FOLDER/hosts $DST_FOLDER/etc/
-sudo cp -p $CONF_FOLDER/interfaces $DST_FOLDER/etc/network/
-sudo cp -p $CONF_FOLDER/neighbor.conf $DST_FOLDER/home/nclab/wpa/
-sudo cp -p $CONF_FOLDER/start_fsm.sh $DST_FOLDER/home/nclab/fsm/
+mkdir -p ${DST_FOLDER}
+
+sudo cp -p ${CONF_FOLDER}/bridge.sh ${DST_FOLDER}/home/nclab/bridge/
+sudo cp -p ${CONF_FOLDER}/check_hostapd.sh ${DST_FOLDER}/home/nclab/hostapd/
+sudo cp -p ${CONF_FOLDER}/check_wpa.sh ${DST_FOLDER}/home/nclab/wpa/
+sudo cp -p ${CONF_FOLDER}/default.conf ${DST_FOLDER}/home/nclab/wpa/
+sudo cp -p ${CONF_FOLDER}/default.conf ${DST_FOLDER}/home/nclab/wpa/wpa_supplicant.conf
+sudo cp -p ${CONF_FOLDER}/hopover.conf ${DST_FOLDER}/home/nclab/wpa/
+sudo cp -p ${CONF_FOLDER}/hostapd.conf ${DST_FOLDER}/home/nclab/hostapd/
+sudo cp -p ${CONF_FOLDER}/hostname ${DST_FOLDER}/etc/
+sudo cp -p ${CONF_FOLDER}/hosts ${DST_FOLDER}/etc/
+sudo cp -p ${CONF_FOLDER}/interfaces ${DST_FOLDER}/etc/network/
+sudo cp -p ${CONF_FOLDER}/neighbor.conf ${DST_FOLDER}/home/nclab/wpa/
+sudo cp -p ${CONF_FOLDER}/start_fsm.sh ${DST_FOLDER}/home/nclab/fsm/
+
+sudo umount ${ROOT_FS}
+sudo sync
